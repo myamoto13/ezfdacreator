d84705959e931675336179205fd25c26a5e1c54d
im.michael.cortes <im.michael.cortes@gmail.com>
2013-02-26 10:02:34 +0000

    First commit (code prototype)

---
+++ FDAProcessor/src/main/java/com/extia/io/excel/data/Jaretiere.java
@@ -0,0 +1,50 @@
+package com.extia.io.excel.data;
+
+public class Jaretiere {
+
+	private String tenant;
+	private String aboutissant;
+	private String ref;
+	private String etat;
+	private String commentaires;
+	
+	public String getTenant() {
+		return tenant;
+	}
+	public void setTenant(String tenant) {
+		this.tenant = tenant;
+	}
+	public String getAboutissant() {
+		return aboutissant;
+	}
+	public void setAboutissant(String aboutissant) {
+		this.aboutissant = aboutissant;
+	}
+	public String getRef() {
+		return ref;
+	}
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+	public String getEtat() {
+		return etat;
+	}
+	public void setEtat(String etat) {
+		this.etat = etat;
+	}
+	public String getCommentaires() {
+		return commentaires;
+	}
+	public void setCommentaires(String commentaires) {
+		this.commentaires = commentaires;
+	}
+	@Override
+	public String toString() {
+		return "Jaretiere [tenant=" + tenant + ", aboutissant=" + aboutissant
+				+ ", ref=" + ref + ", etat=" + etat + ", commentaires="
+				+ commentaires + "]";
+	}
+	
+	
+	
+}
